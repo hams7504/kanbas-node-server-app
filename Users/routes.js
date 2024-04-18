f32722443d966b850ef636a26f9cc4b962fc1906
hams7504 <malli.hamsini@gmail.com>
2024-04-18 01:33:35 -0400

    small fixes

--- Users/routes.js
+++ Users/routes.js
@@ -57,7 +57,6 @@ export default function UserRoutes(app) {
       };
     
     const profile = async (req, res) => {
-        console.log(req.session)
         const currentUser = req.session["currentUser"];
     if (!currentUser) {
       res.sendStatus(401);
